40e0afb5e563fd5b7baebfef076a69cce3a2ab2d
Apurba Ghosh <new.to.world2003@gmail.com>
2021-10-17 17:58:48 +0530

    Commitied the LAst Peoject where the Settings.py is confuigured

---
+++ tubers/webpages/views.py
@@ -0,0 +1,17 @@
+from django.shortcuts import render
+
+# Create your views here.
+
+
+def home(request):
+    pass
+
+def about(request):
+    pass
+
+def services(request):
+    pass
+
+def contact(request):
+    pass
+
